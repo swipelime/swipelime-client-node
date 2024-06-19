9d090b1d83ba5a3e099f6ff05169601113402ff7
Adam L <adam.laky@outlook.com>
2024-06-19 11:24:16 +0200

    Update client version

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ export class Client
 	private _isConnected = false;
 	private _serviceHandlers: ServiceHandler[] = [];
 	public readonly apiVersion = 1;
-	public readonly clientVersion = '0.2.6';
+	public readonly clientVersion = '0.2.7';
 
 	public get isLoggedIn(): boolean
 	{
